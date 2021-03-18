5fa911deb326b6c5351220de381a326e5eb3fc06
Kara <palumbokara@yahoo.com>
2021-03-18 08:59:54 -0600

    refactored to jQuery

--- static/pets.js
+++ static/pets.js
@@ -1,9 +1,41 @@
 const BASE_URL = "https://api.petfinder.com/v2/animals";
+const API_CLIENT_KEY = "56echxaStqqEbshW5qM7UiIDncLPF96oxy7BXnSGaIublt9wf4";
+const API_SECRET_KEY = "cBpMYemJe2WziCouYm7eoKGbYdH5VaoSwlI0NcIu";
+const petForm = $("#pet-form");
+let petCard = $("#pets");
+const default_image =
+  "https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif";
 
-async function getPets() {
-  let res = await axios.get(BASE_URL);
-  console.log(res);
-  return res.data;
+let pf = new petfinder.Client({
+  apiKey: API_CLIENT_KEY,
+  secret: API_SECRET_KEY,
+});
+
+function renderPets(pets) {
+  for (let i = 0; i < pets.length; i++) {
+    console.log(pets[i]);
+    let pet = `<div class="card m-1" style="width: 18rem;">
+    <img src="${default_image}" class="card-img-top m-1" alt="image of pets">
+    <div class="card-body">
+    <h5 class="card-title">${pets[i].name}</h5>
+    <p class="card-text">${pets[i].type}</p>
+    <a href="#" class="btn btn-primary">About me!</a>
+    </div>
+    </div>`;
+    petCard.append(pet);
+  }
 }
 
-getPets();
+function handleResponse(evt) {
+  evt.preventDefault();
+  pf.animal
+    .search()
+    .then(function (response) {
+      renderPets(response.data.animals);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+$("#pet-form").on("submit", handleResponse);
